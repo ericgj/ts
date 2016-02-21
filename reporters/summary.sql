b3f215b3ae223209d5faac3424e754ba4f6afa5c
Eric Gjertsen <ericgj72@gmail.com>
2016-02-20 22:22:35 -0500

    fix summary.sql subqueries
      I never did understand how the damn HAVING clause works

--- reporters/summary.sql
+++ reporters/summary.sql
@@ -8,17 +8,21 @@ CREATE TEMPORARY VIEW byall AS
 CREATE TEMPORARY VIEW byweek AS
   SELECT category, sum(tot_mins) AS tot_mins,
          printf("%d:%02d", sum(tot_mins)/60, sum(tot_mins)%60) as time 
-    FROM timesheets
+    FROM (
+      SELECT * FROM timesheets
+        WHERE start_weeks_ago < 1
+    )
     GROUP BY category
-    HAVING start_weeks_ago < 1
 ;
 
 CREATE TEMPORARY VIEW bytoday AS
   SELECT category, sum(tot_mins) AS tot_mins,
          printf("%d:%02d", sum(tot_mins)/60, sum(tot_mins)%60) as time 
-    FROM timesheets
+    FROM (
+      SELECT * FROM timesheets
+        WHERE start_days_ago = 0
+    )
     GROUP BY category
-    HAVING start_days_ago = 0
 ;
 
 SELECT byall.category, byall.time as time_all, byweek.time as time_week, bytoday.time as time_today
